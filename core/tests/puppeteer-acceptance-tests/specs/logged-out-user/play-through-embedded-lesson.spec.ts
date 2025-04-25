b426cdb062365df72c9e7f64bb751761db1dd67e
root <root@android18>
2025-04-25 16:12:50 -0400

    acceptancetest file update

--- core/tests/puppeteer-acceptance-tests/specs/logged-out-user/play-through-embedded-lesson.spec.ts
+++ core/tests/puppeteer-acceptance-tests/specs/logged-out-user/play-through-embedded-lesson.spec.ts
@@ -13,82 +13,80 @@
 // limitations under the License.
 
 /**
- * @fileoverview Acceptance tests checking if a logged out user can play through an embedded lesson.
+ * @fileoverview Acceptance test for checking if a logged-out user
+ * can play through embedded lessons.
  */
 
 import {UserFactory} from '../../utilities/common/user-factory';
 import testConstants from '../../utilities/common/test-constants';
-import {LoggedOutUser} from '../../utilities/user/logged-out-user;
+import {LoggedOutUser} from '../../utilities/user/logged-out-user';
 import {ConsoleReporter} from '../../utilities/common/console-reporter';
 
 const DEFAULT_SPEC_TIMEOUT_MSECS = testConstants.DEFAULT_SPEC_TIMEOUT_MSECS;
 
 describe('Logged-out User', function () {
-	let loggedOutUser : LoggedOutUser;
+  let loggedOutUser: LoggedOutUser;
 
-	beforeAll(async function () {
-		loggedOutUser = await UserFactory.creatLoggedOutUSer()	
-	},DEFAULT_SPEC_TIMEOUT_MSECS);
-//1
-	it(
-		'should be able to play an embedded lesson in another language and chnage the language by returning to the introduction page.',
-		async function () {
-			//some await
-		}, DEFAULT_SPEC_TIMEOUT_MSECS
-	);
+  beforeAll(async function () {
+    loggedOutUser = await UserFactory.createLoggedOutUser();
+  }, DEFAULT_SPEC_TIMEOUT_MSECS);
 
-//2
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
+  it(
+    'should be able to play embedded lesson and change the language by returning to the introduction page.',
+    async function () {
+      await loggedOutUser.embedThisLesson();
+      await loggedOutUser.changeLessonLanguage();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
+  it(
+    'should be able to navigate to previously-played cards.',
+    async function () {
+      await loggedOutUser.goBackToPreviousCard();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
+  it(
+    'should not be able to answer a previously-answered question.',
+    async function () {
+     await loggedOutUser.verifyCannotnswerPrevioulyAnsreQuestion();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
-}
-)
-//3
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
+  it(
+    'should be able to use concept cards and hints wherever provided.',
+    async function () {
+      //await loggedOutUser.<enter function>();
+      //await loggedOutUser.<enter function>();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
-//4
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
+  it(
+    'should be able to view all the previous responses to any question.',
+    async function () {
+      //await loggedOutUser.<enter function>();
+      //await loggedOutUser.<enter function>();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
-//5
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
+  it(
+    'should be able to restart the exploration upon refreshing the page.',
+    async function () {
+      await loggedOutUser.<enter function>();
+      await loggedOutUser.<enter function>();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
-//6
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
+  it(
+    'should receive a confirmation via a toast message upon completeing the exploration.',
+    async function () {
+      await loggedOutUser.expectExplorationCompletionToastMessage(await this.page.waitForSelector(explorationCompletionToastMessage);();
+      await loggedOutUser.<enter function>();
+    }, DEFAULT_SPEC_TIMEOUT_MSECS
+  );
 
-//7
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
-
-//8
- it(
-                'should be able to play an embedded lesson in a$                async function () {
-                        //some await
-                }, DEFAULT_SPEC_TIMEOUT_MSECS
-        );
-afterAll(async function () {	
-await UserFactory.closeAllBrowsers();});
+  afterAll(async function () {
+    await UserFactory.closeAllBrowsers();
+  });
 });
+           
